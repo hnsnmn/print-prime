a8dd93fd4194780a0ec4e60b00dcfa82ae136549
{hongseongmin} <{hnsnmn@gmail.com}>
2014-01-22 18:09:51 +0900

    PrintPrimesTest - add failing test for main method

---
+++ src/test/java/function/FileContentComparator.java
@@ -0,0 +1,11 @@
+package function;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 1. 22.
+ * Time: 오후 6:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class FileContentComparator {
+}
---
+++ src/test/java/function/PrintPrimesTest.java
@@ -0,0 +1,17 @@
+package function;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 1. 22.
+ * Time: 오후 6:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class PrintPrimesTest {
+	@Test
+	public void shuld_print_result() {
+		PrintPrimes.main(new String[0]);
+	}
+}
