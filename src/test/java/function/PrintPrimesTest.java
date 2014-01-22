0b7c1d729a46161ee454cf0a7064a1992b97026e
{hongseongmin} <{hnsnmn@gmail.com}>
2014-01-22 22:28:04 +0900

    rename should_print_result();

--- src/test/java/function/PrintPrimesTest.java
+++ src/test/java/function/PrintPrimesTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
  */
 public class PrintPrimesTest {
 	@Test
-	public void shuld_print_result() {
+	public void should_print_result() {
 		PrintPrimes.main(new String[0]);
 	}
 }
