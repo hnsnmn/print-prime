086fdfa0930059be8157f5b8db4693f30d722f1c
{hongseongmin} <{hnsnmn@gmail.com}>
2014-01-22 22:46:41 +0900

    add characterization test - PrintPrimes.main을 수행시켜 화면에 출력된 결과를 lead 파일로 저 장 - setUp에서 System.out을 lead로 치환 - gold와 lead 파일 내용을 비교

--- src/test/java/function/PrintPrimesTest.java
+++ src/test/java/function/PrintPrimesTest.java
@@ -1,6 +1,12 @@
 package function;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.*;
+
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,8 +16,29 @@ import org.junit.Test;
  * To change this template use File | Settings | File Templates.
  */
 public class PrintPrimesTest {
+	private PrintStream out;
+
+	@Before
+	public void setUp() throws FileNotFoundException {
+		out = System.out;
+		System.setOut(new PrintStream(new FileOutputStream("lead")));
+	}
+
+	@After
+	public void tearDown() {
+		System.setOut(out);
+		new File("lead").delete();
+	}
+
+
 	@Test
-	public void should_print_result() {
+	public void makeSureMatchesGold() throws IOException {
 		PrintPrimes.main(new String[0]);
+		BufferedReader lead = new BufferedReader(new FileReader("lead"));
+		BufferedReader gold = new BufferedReader(new FileReader("src/test/java/function/gold"));
+		String line;
+		while ((line = gold.readLine()) != null)
+			assertEquals(line, lead.readLine());
+		assertEquals(null, lead.readLine());
 	}
 }
