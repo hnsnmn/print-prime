8efc7e7b479f929f93c5f57e0fca6420d37d29eb
{hongseongmin} <{hnsnmn@gmail.com}>
2014-01-22 22:27:23 +0900

    FileContentComparator - add class

--- src/test/java/function/FileContentComparator.java
+++ src/test/java/function/FileContentComparator.java
@@ -1,5 +1,10 @@
 package function;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created with IntelliJ IDEA.
  * User: hongseongmin
@@ -8,5 +13,18 @@ package function;
  * To change this template use File | Settings | File Templates.
  */
 public class FileContentComparator {
+	public void compare() {
+		try {
+			BufferedReader lead = new BufferedReader(new FileReader("lead"));
+			BufferedReader gold = new BufferedReader(new FileReader("src/test/java/function/gold"));
+			String line;
+
+			while ((line = gold.readLine()) != null)
+				assertEquals(line, lead.readLine());
+			assertEquals(null, lead.readLine());
+		} catch (Exception e) {
+			e.printStackTrace();	// To change body of catch statement use File | Settings | File Templates
+		}
+	}
 
 }
